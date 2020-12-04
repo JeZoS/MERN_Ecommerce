ae42b29845ec122bd3f59d62223554c271077e87
JeZoS <prabhat262111@gmail.com>
2020-12-04 18:00:43 +0530

    Connected frontend and backend

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
-import products from "../products";
+import Axios from "axios";
 
 const ProductScreen = ({ match }) => {
-  const product = products.find((p) => p._id === match.params.id);
-  console.log(product);
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const fetchAndSet = async () => {
+      const res = await Axios.get(`/api/products/${match.params.id}`);
+      setProduct(res.data);
+    };
+    fetchAndSet();
+  }, [match]);
   return (
     <>
       <Link className="btn btn-light my-3" to="/">
